dd450259d78045feed106a043b75cd6cdc7d5139
Doad Software Department <98625971+DoadSoftware@users.noreply.github.com>
2023-02-06 19:02:31 +0530

    update

--- src/main/java/com/football/util/FootballUtil.java
+++ src/main/java/com/football/util/FootballUtil.java
@@ -4,8 +4,9 @@ public class FootballUtil {
 
 	public final static String DOAD = "DOAD";
 	
-//	public final static String FOOTBALL_DIRECTORY = "//DOAD-SERVER2//C//Sports//Football//";
 	public final static String FOOTBALL_DIRECTORY = "C:/Sports/Football/";
+//	public final static String FOOTBALL_DIRECTORY = "//DOAD-SERVER2//C//Sports//Football//";
+	public final static String FOOTBALL_CONFIG_DIRECTORY = "C:/Sports/Football/";
 	public final static String EVENT_DIRECTORY = "Event/";
 	public final static String MATCHES_DIRECTORY = "Matches/";
 	public final static String SCENES_DIRECTORY = "Scenes/";
